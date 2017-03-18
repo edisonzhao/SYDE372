70ef7d12a037ce7ac7025bacdf6d19e4649f69ff
piggyandy <ch3au@uwaterloo.ca>
2017-03-18 17:07:41 -0400

    fixed MED

--- Lab2/Part 4/MED.m
+++ Lab2/Part 4/MED.m
@@ -1,9 +1,10 @@
 function [ class_id ] = MED( class1, class2, x )
-    val = (class2-class1)' * x + 0.5*(class1' * class1 - class2' * class2);
+    %val = (class2-class1)' * x + 0.5*(class1' * class1 - class2' * class2);
+    val = (x-class1)' * (x-class1) - (x - class2)' * (x - class2);
     if val > 0
-        class_id = 1;
-    else 
         class_id = 2;
+    else 
+        class_id = 1;
     end
 end
 
